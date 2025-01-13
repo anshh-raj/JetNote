c432fe73efdc42e8d8a4ab9532048609a110c9ef
anshh-raj <anshambastha@gmail.com>
2025-01-14 03:39:17 +0530

    done basic setup and added textfield

---
+++ app/src/main/java/com/example/jetnote/screen/NoteScreen.kt
@@ -0,0 +1,102 @@
+package com.example.jetnote.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetnote.R
+import com.example.jetnote.components.NoteButton
+import com.example.jetnote.components.NoteInputText
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteScreen(){
+    var title by remember {
+        mutableStateOf("")
+    }
+    var description by remember {
+        mutableStateOf("")
+    }
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name)
+                    )
+                },
+                actions = {
+                    Icon(
+                        imageVector = Icons.Rounded.Notifications,
+                        contentDescription = "Icon"
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFFDADFE3)
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            NoteInputText(
+                text = title,
+                label = "Title",
+                onTextChange = {
+                    if (it.all{ char->
+                        char.isLetter() || char.isWhitespace()
+                        }) title = it
+                },
+                modifier = Modifier
+                    .padding(top = 9.dp, bottom = 8.dp)
+            )
+            NoteInputText(
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all{ char->
+                            char.isLetter() || char.isWhitespace()
+                        }) description = it
+                },
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp)
+            )
+            NoteButton(
+                text = "Save",
+                onClick = {
+
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NotesScreenPreview(){
+    NoteScreen()
+}
